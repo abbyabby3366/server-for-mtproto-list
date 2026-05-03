18b0c50e9783f0f80da91cbd68c8d3d3b3218ce8
Desmond Giam <desmondgiam@gmail.com>
2026-05-03 19:11:25 +0800

    fixed htmls

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const NetworkTelemetry = mongoose.model('NetworkTelemetry', networkTelemetrySche
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
 
 // Optional API key authentication
 const apiKeyAuth = (req, res, next) => {
